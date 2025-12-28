aa1aabef5c176d6b4fb1289f8e579d5ecabae5af
Sujan Khatri <sujan.chettri1321@gmail.com>
2025-12-28 05:09:13 +0000

    feat: Update environment variables, enhance Filter functionality, and modify test structure

--- Pages/Filter.ts
+++ Pages/Filter.ts
@@ -2,8 +2,19 @@ import { Page } from "@playwright/test";
 import { parse } from 'csv-parse/sync';
 import fs from 'fs';
 
+
 const testdatacsv = './Data/testdata.csv';
 fs.readFileSync(testdatacsv, 'utf-8');
 const readfile = fs.readFileSync(testdatacsv, 'utf-8');
-const records = parse(readfile, { columns: true, skip_empty_lines: true });
+const records = parse(readfile, { columns: true, skip_empty_lines: true }) as Array<{ ClientCode: string }>;
+const loginURL = process.env.LoginURL!;
+export class FilterPage {
+    constructor(private page: Page) { }
+    async applyClientCodeFilter(clientcodenumber: number) {
+        await this.page.goto(process.env.Bills!);
+        const clientcode_filter = await this.page.locator('#controllable-states-demo').first();
+        await clientcode_filter.fill(`${records[clientcodenumber].ClientCode}`);
+        const click_option = await this.page.locator('#controllable-states-demo-option-0').click();
 
+    }
+}
--- tests/filter.spec.ts
+++ tests/filter.spec.ts
@@ -1,6 +1,8 @@
 import { parse } from 'csv-parse/sync';
 import fs from 'fs';
 import { test, expect } from "@playwright/test"
+import "dotenv/config";
+import { FilterPage } from '../Pages/Filter';
 
 const testdatacsv = './Data/testdata.csv';
 fs.readFileSync(testdatacsv, 'utf-8');
@@ -9,9 +11,17 @@ const records = parse(readfile, { columns: true, skip_empty_lines: true }) as Ar
 
 const loginURL = process.env.LoginURL!;
 
-test(' Filter Test', async ({ page }) => {
-  await page.goto(loginURL);
-  const clientcode_filter = await page.locator('#controllable-states-demo');
-  await clientcode_filter.fill(`${records[0].ClientCode}`);
+test.describe('Filter Tests', () => {
+// test(' Filter Test for client code', async ({ page }) => {
+//   console.log(process.env.Bills!);
+//   await page.goto(process.env.Bills!);
+//   const clientcode_filter = await page.locator('#controllable-states-demo').first();
+//   await clientcode_filter.fill(`${records[0].ClientCode}`);
+//   await page.getByRole('button', { name: 'Filter' }).click();
+// });
 
+test(' Filter Test for invalid client code', async ({ page }) => {
+  const ClinetFilter=new FilterPage(page);
+  await ClinetFilter.applyClientCodeFilter(2);
+})
 });
